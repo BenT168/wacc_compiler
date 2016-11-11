5adec2dd15071352d504459be1c2909c2205690b
Ben Tan <bst15@graphic26.doc.ic.ac.uk>
2016-11-11 10:56:24 +0000

    [bst15]:modification of main

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,78 @@ import antlr.WACCLexer;
 import antlr.WACCParser;
 import frontEnd.visitor.TypeCheckVisitor;
 import org.antlr.v4.runtime.ANTLRInputStream;
-import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.misc.ParseCancellationException;
-import org.antlr.v4.runtime.tree.ParseTree;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Main {
 
-    public static void main(String[] args) throws ParseCancellationException{
+    public static void main(String[] args) throws IOException {
+
+        String inputFile = null;
+        if (args.length > 0) {
+            inputFile = args[0];
+        }
+        InputStream is = System.in;
+        if (inputFile != null) {
+            is = new FileInputStream(inputFile);
+        }
+
+
+        ANTLRInputStream input = new ANTLRInputStream(is);
+
+        /* Create a lexer that feeds off of input CharStream */
+        WACCLexer lexer = new WACCLexer(input);
+
+        /* Create a buffer of tokens pulled from the lexer */
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        /* Create a parser that feeds off the tokens buffer */
+        WACCParser parser = new WACCParser(tokens);
+
+        /* Generate AST, begin parsing at the program rule */
+        WACCParser.ProgramContext tree = parser.program();
+
+        /* Checking Syntax */
+        if (parser.getNumberOfSyntaxErrors() == 0) {
+
+            /* Checking Semantics */
+            try {
+                System.out.println("The visitor visits every nodes of AST");
+                TypeCheckVisitor visitor = new TypeCheckVisitor();
+                System.out.println("====");
+            } catch (Exception e) {
+                if (e instanceof NullPointerException) {
+                    System.exit(0);
+                } else {
+                    System.exit(200);
+                }
+            }
+            /*!visitor.semanticError*/
+
+        } else {
+            System.out.println("SYNTAX ERROR: "
+                    + parser.getNumberOfSyntaxErrors());
+
+            /* A compilation that fails due to one or
+                more syntax errors should return the exit status 100 */
+            System.exit(100);
+        }
+
+        /* A successful compilation should return the exit status 0 */
+        System.exit(0);
+    }
+}
+
+
+
 
         /* Check if Argument is given, throw error if not the right number
          */
+
+        /*
         if(args.length != 2) {
             System.out.println("Error: One Argument Should be given.");
             System.exit(-1);
@@ -26,6 +83,7 @@ public class Main {
 
         /* Check if file exists
          */
+        /*
         if(!file.exists()) {
             System.out.println("Error: File inputed does not exist.");
             System.exit(-1);
@@ -44,6 +102,7 @@ public class Main {
 
             /*Check if there are any Syntatic errors
              */
+        /*
             int syntaxErr = parser.getNumberOfSyntaxErrors();
             if(syntaxErr > 0) {
                 System.exit(100);
@@ -61,3 +120,4 @@ public class Main {
         }
     }
 }
+*/
