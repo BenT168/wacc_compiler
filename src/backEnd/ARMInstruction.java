cf6697b7305852118d5b89f53ddaf44719685385
ai2015 <alessio.incitti15@imperial.ac.uk>
2016-11-21 18:31:31 +0000

    [ai2015]: Added instruction templates to ARMInstruction.java

--- src/backEnd/ARMInstruction.java
+++ src/backEnd/ARMInstruction.java
@@ -1,43 +1,82 @@
 package backEnd;
 
 
-public class ARMInstruction {
+class ARMInstruction {
 
-    public static String pushNew() {
+    static String pushNew() {
         return "PUSH {lr}";
     }
 
-    public static String ret() {
+    static String ret() {
         return "POP {pc}";
     }
 
-    public static String push(String reg) {
-        return "PUSH {"+ reg +"}";
+    static String push(String reg) {
+        return "PUSH {" + reg + "}";
     }
 
-    public static String pop(String reg) {
-        return "POP {"+ reg +"}";
+    static String pop(String reg) {
+        return "POP {" + reg + "}";
     }
 
-    public static String ldr(String reg, String val) {
-        return "LDR "+ reg +", ="+val;
+    static String branchCond(String string, String condition) {
+        return "BL{" + condition + "} " + string;
     }
 
-    public static String bl(String string) {
-        return "BL "+string;
+    static String branch(String string) {
+        return "B " + string;
     }
 
-    public static String mov(String reg, String val) {
-        return "MOV "+ reg +", #"+val;
+    static String branchwlink(String string) {
+        return "BL " + string;
     }
 
-    public static String sub(String store, String r1, String val) {
-        return "SUB "+ store +", "+r1+", #"+val;
+    static String mov(String reg, int imm) {
+        return "MOV " + reg + ", #" + imm;
     }
 
-    public static String add(String store, String r1, String val) {
-        return "ADD "+ store +", "+r1+", #"+val;
+    static String mov(String dstreg, String reg) {
+        return "MOV " + dstreg + ", " + reg;
     }
 
+    static String movEQ(String dstreg, String reg) {
+        return "MOVEQ " + dstreg + ", " + reg;
+    }
+
+    static String movNE(String dstreg, String reg) {
+        return "MOVNE " + dstreg + ", " + reg;
+    }
+
+    static String sub(String store, String r1, int imm) {
+        return "SUB " + store + ", " + r1 + ", #" + imm;
+    }
+
+    static String add(String store, String r1, int imm) {
+        return "ADD " + store + ", " + r1 + ", #" + imm;
+    }
+
+    static String ldr(String dstreg, String val) {
+        return "LDR " + dstreg + ", =" + val;
+    }
+
+    static String ldr(String dstreg, String reg, int offset) {
+        return "LDR " + dstreg + ", [" + reg + ", " + offset + "]";
+    }
+
+    static String str(String dstreg, String src) {
+        return "STR " + dstreg + " [" + src + "]";
+    }
+
+    static String str(String dstreg, String src, int offset) {
+        return "STR " + dstreg + " [" + src + ", #" + offset + "]";
+    }
+
+    static String cmp(String reg1, String reg2) {
+        return "CMP " + reg1 + ", " + reg2;
+    }
+
+    static String cmp(String reg1, int imm) {
+        return "CMP " + reg1 + ", #" + imm;
+    }
 
 }
