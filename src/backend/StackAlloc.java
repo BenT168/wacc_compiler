a4d735271488c0d806e761016af2e8f043d0c3c7
ai2015 <alessio.incitti15@imperial.ac.uk>
2016-12-06 18:21:43 +0000

    [ai2015]: Added meaningful commenting for most of back-end methods

--- src/backend/StackAlloc.java
+++ src/backend/StackAlloc.java
@@ -15,42 +15,50 @@ public class StackAlloc {
 
 	private Stack<Integer> scopeStack;
 
+	/* constructor */
 	public StackAlloc() {
 		scopeStack = new Stack<>();
 		scopeStack.push(0);
 	}
 
+	/* returns stack position to allocate to */
 	public StackPos OnStackAlloc() {
 		StackPos pos = new StackPos(getCounter());
 		incCounter();
 		return pos;
 	}
 
+	/* increments counter */
 	private void incCounter() {
-		scopeStack.push( scopeStack.pop() + 1 );
+		scopeStack.push(scopeStack.pop() + 1);
 	}
 
+	/* returns current counter */
 	public int getCounter() {
 		return scopeStack.peek();
 	}
 
+	/* to be called every time a new scope is entered */
 	public void enterNewScope() {
 		scopeStack.push(0);
 	}
 
+	/* to be called every time a scope is exited */
 	public void exitScope() {
 		scopeStack.pop();
 	}
 
+	/* returns initialised token sequence */
 	public TokSeq getInit() {
 		return getSeq(new SubToken(null, null, 0));
 	}
 
+	/* returns terminated token sequence */
 	public TokSeq getTerminated() {
 		return getSeq(new AddImmToken(null, null, null));
 	}
 
-
+	/* returns a sequence created starting from the token passed as argument onwards */
 	private TokSeq getSeq(Token sampleToken) {
 		TokSeq seq = new TokSeq();
 		int remaining = getCounter() * WORD_SIZE;
