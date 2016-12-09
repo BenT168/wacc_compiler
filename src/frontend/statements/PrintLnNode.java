00d2498c1d4849ed0ef74abdb1a8981e7154e846
Ben Tan <bst15@line24.doc.ic.ac.uk>
2016-12-09 18:55:47 +0000

    [bst15}: removal of unused code

--- src/frontend/statements/PrintLnNode.java
+++ src/frontend/statements/PrintLnNode.java
@@ -22,10 +22,7 @@ public class PrintLnNode extends StatNode {
 
 	@Override
 	public TokSeq assemblyCodeGenerating(Register register) {
-		TokSeq exprSeq = new TokSeq();
-		//if(!ex_ForLoopNode.isInLoopAssembler()) {
-			exprSeq = expr.assemblyCodeGenerating(register);
-		//}
+		TokSeq exprSeq = expr.assemblyCodeGenerating(register);
 		TokSeq printSeq = expr.getType().printAssembly(register);
 		Token println = new PrintLnToken();
 		exprSeq.appendAll(printSeq);
